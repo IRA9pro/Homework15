fe9c2de3e2658f8e4d34f3a876dcccf129093a82
IRA9pro <ira9program@gmail.com>
2025-10-18 16:11:44 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework15/MainActivity.java
@@ -0,0 +1,39 @@
+  package com.example.homework15;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+
+  public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView imageView = findViewById(R.id.btn_image);
+
+        Glide.with(this)
+                .load("https://cdn.pixabay.com/animation/2023/04/06/16/10/16-10-43-442_512.gif")
+                .into(imageView);
+
+        imageView.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(intent);
+        });
+    }
+}
